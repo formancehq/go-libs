9d6ff19c1eb641dfd7d973e93c0620ff3e0e3d0c
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2022-02-24 14:09:19 +0100

    Add base api responses.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/numary/go-libs
+
+go 1.17
---
+++ sharedapi/response.go
@@ -0,0 +1,16 @@
+package sharedapi
+
+type BaseResponse struct {
+	Data   interface{} `json:"data,omitempty"`
+	Cursor *Cursor     `json:"cursor,omitempty"`
+}
+
+type Cursor struct {
+	PageSize  int         `json:"page_size,omitempty"`
+	HasMore   bool        `json:"has_more"`
+	Total     int64       `json:"total,omitempty"`
+	Remaining int         `json:"remaining_results,omitempty"`
+	Previous  string      `json:"previous,omitempty"`
+	Next      string      `json:"next,omitempty"`
+	Data      interface{} `json:"data"`
+}
