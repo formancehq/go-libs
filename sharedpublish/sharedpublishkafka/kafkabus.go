02604f34c81f18556f3b5d4dfba614477dcffb6e
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2022-03-25 09:04:12 +0100

    Some renaming and add tls option on kafka publish.

--- sharedpublish/sharedpublishkafka/kafkabus.go
+++ sharedpublish/sharedpublishkafka/kafkabus.go
@@ -1,6 +1,7 @@
 package sharedpublishkafka
 
 import (
+	"crypto/tls"
 	"github.com/Shopify/sarama"
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill-kafka/v2/pkg/kafka"
@@ -29,28 +30,40 @@ func WithProducerReturnSuccess() SaramaOptionFn {
 	}
 }
 
-func WithNetSASLEnabled() SaramaOptionFn {
+func WithSASLEnabled() SaramaOptionFn {
 	return func(config *sarama.Config) {
 		config.Net.SASL.Enable = true
 	}
 }
 
-func WithNetSASLMechanism(mechanism sarama.SASLMechanism) SaramaOptionFn {
+func WithSASLMechanism(mechanism sarama.SASLMechanism) SaramaOptionFn {
 	return func(config *sarama.Config) {
 		config.Net.SASL.Mechanism = mechanism
 	}
 }
 
-func WithNetSASLScramClient(fn func() sarama.SCRAMClient) SaramaOptionFn {
+func WithSASLScramClient(fn func() sarama.SCRAMClient) SaramaOptionFn {
 	return func(config *sarama.Config) {
 		config.Net.SASL.SCRAMClientGeneratorFunc = fn
 	}
 }
 
-func WithNetSASLCredentials(user, pwd string) SaramaOptionFn {
+func WithSASLCredentials(user, pwd string) SaramaOptionFn {
 	return func(config *sarama.Config) {
 		config.Net.SASL.User = user
 		config.Net.SASL.Password = pwd
+	}
+}
+
+func WithTLS() SaramaOptionFn {
+	return func(config *sarama.Config) {
+		config.Net.TLS = struct {
+			Enable bool
+			Config *tls.Config
+		}{
+			Enable: true,
+			Config: &tls.Config{},
+		}
 	}
 }
 
