f2a5122cde4179b23a2f14a545abac319b7f78c0
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2022-04-24 17:54:50 +0200

    Add a publisher capable of mapping topics.

--- go.mod
+++ go.mod
@@ -57,6 +57,7 @@ require (
 	github.com/klauspost/compress v1.14.4 // indirect
 	github.com/lithammer/shortuuid/v3 v3.0.4 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/pborman/uuid v1.2.1 // indirect
 	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
---
+++ sharedpublish/publisher.go
@@ -0,0 +1,61 @@
+package sharedpublish
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/numary/go-libs/sharedlogging"
+	"github.com/pborman/uuid"
+	"go.uber.org/fx"
+)
+
+// TODO: Inject OpenTracing context
+func newMessage(ctx context.Context, m interface{}) *message.Message {
+	data, err := json.Marshal(m)
+	if err != nil {
+		panic(err)
+	}
+	msg := message.NewMessage(uuid.New(), data)
+	msg.SetContext(ctx)
+	return msg
+}
+
+type TopicMapperPublisher struct {
+	publisher message.Publisher
+	topics    map[string]string
+}
+
+func (l *TopicMapperPublisher) publish(ctx context.Context, topic string, ev interface{}) error {
+	err := l.publisher.Publish(topic, newMessage(ctx, ev))
+	if err != nil {
+		sharedlogging.GetLogger(ctx).Errorf("Publishing message: %s", err)
+		return err
+	}
+	return nil
+}
+
+func (l *TopicMapperPublisher) Publish(ctx context.Context, topic string, ev interface{}) error {
+	mappedTopic, ok := l.topics[topic]
+	if ok {
+		return l.publish(ctx, mappedTopic, ev)
+	}
+	mappedTopic, ok = l.topics["*"]
+	if ok {
+		return l.publish(ctx, mappedTopic, ev)
+	}
+
+	return l.publish(ctx, topic, ev)
+}
+
+func NewTopicMapperPublisher(publisher message.Publisher, topics map[string]string) *TopicMapperPublisher {
+	return &TopicMapperPublisher{
+		publisher: publisher,
+		topics:    topics,
+	}
+}
+
+func TopicMapperPublisherModule(topics map[string]string) fx.Option {
+	return fx.Provide(func(p message.Publisher) *TopicMapperPublisher {
+		return NewTopicMapperPublisher(p, topics)
+	})
+}
