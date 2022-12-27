c279e11f6575ce9113f64cf6234e3ebb37134c2f
Antoine Gelloz <antoine.gelloz@me.com>
2022-12-27 13:56:33 +0100

    chore: add deprecated

--- sharedapi/response.go
+++ sharedapi/response.go
@@ -24,6 +24,14 @@ type ErrorResponse struct {
 	ErrorMessage string `json:"errorMessage,omitempty"`
 }
 
+// BaseResponseDeprecated is deprecated: use sharedapi.BaseResponse instead
+//
+// TODO: to be removed after a v2 release of the ledger
+type BaseResponseDeprecated[T any] struct {
+	Data   *T                   `json:"data,omitempty"`
+	Cursor *CursorDeprecated[T] `json:"cursor,omitempty"`
+}
+
 // CursorDeprecated is deprecated: use sharedapi.Cursor instead
 //
 // TODO: to be removed after a v2 release of the ledger
@@ -33,4 +41,12 @@ type CursorDeprecated[T any] struct {
 	Previous string `json:"previous,omitempty"`
 	Next     string `json:"next,omitempty"`
 	Data     []T    `json:"data"`
+}
+
+// ErrorResponseDeprecated is deprecated: use sharedapi.ErrorResponse instead
+//
+// TODO: to be removed after a v2 release of the ledger
+type ErrorResponseDeprecated struct {
+	ErrorCode    string `json:"error_code,omitempty"`
+	ErrorMessage string `json:"error_message,omitempty"`
 }
