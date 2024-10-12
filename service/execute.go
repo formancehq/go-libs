f512ec11bf14404530e5d18eab572765851ed75d
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2024-10-12 22:14:07 +0200

    feat: remove duplicated error print

--- service/execute.go
+++ service/execute.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -10,9 +9,6 @@ import (
 func Execute(cmd *cobra.Command) {
 	BindEnvToCommand(cmd)
 	if err := cmd.Execute(); err != nil {
-		if _, err := fmt.Fprintln(os.Stderr, err); err != nil {
-			panic(err)
-		}
 		os.Exit(1)
 	}
 }
