334d4072a5486a822e74ab4ead3cd076a789511d
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2024-10-28 10:07:14 +0100

    feat: simplify migrator

--- migrations/test_util.go
+++ migrations/test_util.go
@@ -8,23 +8,22 @@ import (
 	"io/fs"
 
 	"github.com/formancehq/go-libs/v2/pointer"
-	"github.com/uptrace/bun"
 )
 
-func TestMigrations(ctx context.Context, _fs embed.FS, db *bun.DB, migrator *Migrator) error {
+func TestMigrations(ctx context.Context, _fs embed.FS, migrator *Migrator) error {
 	_, err := WalkMigrations(_fs, func(entry fs.DirEntry) (*struct{}, error) {
 		before, err := TemplateSQLFile(_fs, migrator.GetSchema(), entry.Name(), "up_tests_before.sql")
 		if err != nil && !errors.Is(err, fs.ErrNotExist) {
 			return nil, err
 		}
 		if err == nil {
-			_, err = db.ExecContext(ctx, before)
+			_, err = migrator.db.ExecContext(ctx, before)
 			if err != nil {
 				return nil, fmt.Errorf("executing pre migration script: %s", entry.Name())
 			}
 		}
 
-		if err := migrator.UpByOne(ctx, db); err != nil {
+		if err := migrator.UpByOne(ctx); err != nil {
 			switch {
 			case errors.Is(err, ErrAlreadyUpToDate):
 				return nil, nil
@@ -39,7 +38,7 @@ func TestMigrations(ctx context.Context, _fs embed.FS, db *bun.DB, migrator *Mig
 			return nil, err
 		}
 		if err == nil {
-			_, err = db.ExecContext(ctx, after)
+			_, err = migrator.db.ExecContext(ctx, after)
 			if err != nil {
 				return nil, fmt.Errorf("executing post migration script: %s", entry.Name())
 			}
