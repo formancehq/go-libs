fadfe26b4b3a16ed117b43794eb7756393313d20
Crimson <laouji@users.noreply.github.com>
2024-11-15 13:22:37 +0100

    feat: (testing) add temporal dev server testing utility for e2e tests (#179)
    * feat(testing): add temporal dev server testing utility for e2e tests
    
    * fix: add missing server stop call at cleanup

---
+++ testing/platform/temporaltesting/temporal.go
@@ -0,0 +1,49 @@
+package temporaltesting
+
+import (
+	"context"
+	"io"
+
+	"github.com/stretchr/testify/require"
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/testsuite"
+)
+
+var defaultNamespace = "temporaltesting-default-namespace"
+
+type TemporalT interface {
+	require.TestingT
+	Cleanup(func())
+}
+
+type TemporalServer struct {
+	*testsuite.DevServer `json:"-"`
+	defaultNamespace     string
+}
+
+func CreateTemporalServer(t TemporalT, w io.Writer) *TemporalServer {
+	srv, err := testsuite.StartDevServer(context.Background(), testsuite.DevServerOptions{
+		ClientOptions: &client.Options{Namespace: defaultNamespace},
+		Stdout:        w,
+		Stderr:        w,
+	})
+	if err != nil {
+		require.Failf(t, "failed to start temporal dev server: %s", err.Error())
+	}
+	t.Cleanup(func() {
+		_ = srv.Stop()
+	})
+	return &TemporalServer{DevServer: srv, defaultNamespace: defaultNamespace}
+}
+
+func (s *TemporalServer) Client(ctx context.Context) client.Client {
+	return s.Client(ctx)
+}
+
+func (s *TemporalServer) Address() string {
+	return s.FrontendHostPort()
+}
+
+func (s *TemporalServer) DefaultNamespace() string {
+	return s.defaultNamespace
+}
