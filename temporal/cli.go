36a75d6f23c844041c0088b516be8fb6dcbafa31
Paul Nicolas <paul.nicolas.go@gmail.com>
2024-12-09 11:24:15 +0100

    feat(temporal): add max concurrent task pollers option

--- temporal/cli.go
+++ temporal/cli.go
@@ -5,13 +5,14 @@ import (
 )
 
 const (
-	TemporalAddressFlag               = "temporal-address"
-	TemporalNamespaceFlag             = "temporal-namespace"
-	TemporalSSLClientKeyFlag          = "temporal-ssl-client-key"
-	TemporalSSLClientCertFlag         = "temporal-ssl-client-cert"
-	TemporalTaskQueueFlag             = "temporal-task-queue"
-	TemporalInitSearchAttributesFlag  = "temporal-init-search-attributes"
-	TemporalMaxParallelActivitiesFlag = "temporal-max-parallel-activities"
+	TemporalAddressFlag                      = "temporal-address"
+	TemporalNamespaceFlag                    = "temporal-namespace"
+	TemporalSSLClientKeyFlag                 = "temporal-ssl-client-key"
+	TemporalSSLClientCertFlag                = "temporal-ssl-client-cert"
+	TemporalTaskQueueFlag                    = "temporal-task-queue"
+	TemporalInitSearchAttributesFlag         = "temporal-init-search-attributes"
+	TemporalMaxParallelActivitiesFlag        = "temporal-max-parallel-activities"
+	TemporalMaxConcurrentWorkflowTaskPollers = "temporal-max-concurrent-workflow-task-pollers"
 )
 
 func AddFlags(flags *pflag.FlagSet) {
@@ -22,4 +23,7 @@ func AddFlags(flags *pflag.FlagSet) {
 	flags.String(TemporalTaskQueueFlag, "default", "Temporal task queue name")
 	flags.Bool(TemporalInitSearchAttributesFlag, false, "Init temporal search attributes")
 	flags.Float64(TemporalMaxParallelActivitiesFlag, 10, "Maximum number of parallel activities")
+	// MaxConcurrentWorkflowTaskPollers cannot be < 2, otherwise, temporal will panic. 2 is the default
+	// and minimum value.
+	flags.Int(TemporalMaxConcurrentWorkflowTaskPollers, 2, "Maximum number of concurrent workflow task pollers")
 }
