2aa87d319835d4ec79c7dc9ebed2981f24bae364
Paul Nicolas <paul.nicolas.go@gmail.com>
2025-03-11 11:54:52 +0100

    fix(temporal): add tests

--- temporal/encryption_test.go
+++ temporal/encryption_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"go.temporal.io/sdk/converter"
 )
 
 func TestEncryption(t *testing.T) {
@@ -33,6 +34,30 @@ func TestEncryption(t *testing.T) {
 		require.NoError(t, err)
 		require.NotNil(t, p)
 
+		result := make([]Payload, 2)
+		err = converter.FromPayloads(p, &result[0], &result[1])
+		require.NoError(t, err)
+		require.Equal(t, payload[0].Foo, result[0].Foo)
+		require.Equal(t, payload[1].Foo, result[1].Foo)
+	})
+
+	t.Run("decrypt if metadata is not present", func(t *testing.T) {
+		defaultConverter := converter.GetDefaultDataConverter()
+
+		payload := []Payload{
+			{
+				Foo: "bar",
+			},
+			{
+				Foo: "baz",
+			},
+		}
+		p, err := defaultConverter.ToPayloads(&payload[0], &payload[1])
+		require.NoError(t, err)
+		require.NotNil(t, p)
+
+		converter, err := NewEncryptionDataConverter([]byte("12345678901011121314151617181920"))
+		require.NoError(t, err)
 		result := make([]Payload, 2)
 		err = converter.FromPayloads(p, &result[0], &result[1])
 		require.NoError(t, err)
