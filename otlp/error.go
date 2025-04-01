68ddbe71a697929a7395f2931bb134d2b2606dd2
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2025-04-01 10:23:44 +0200

    feat: add traces on migrations

--- otlp/error.go
+++ otlp/error.go
@@ -8,13 +8,16 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
-func RecordError(ctx context.Context, e error) {
+func RecordError(ctx context.Context, e error, opts ...trace.EventOption) {
 	if e == nil {
 		return
 	}
 	span := trace.SpanFromContext(ctx)
+	if span == nil {
+		return
+	}
 	span.SetStatus(codes.Error, e.Error())
-	span.RecordError(e, trace.WithStackTrace(true))
+	span.RecordError(e, append(opts, trace.WithStackTrace(true))...)
 }
 
 func RecordAsError(ctx context.Context, e any) {
