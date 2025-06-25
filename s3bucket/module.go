afb440f860f712a2b1ee8f67cbd02a0c94631701
Crimson Thompson <crimson@formance.com>
2025-06-25 15:07:48 +0200

    Add S3 bucket client module

---
+++ s3bucket/module.go
@@ -0,0 +1,32 @@
+package s3bucket
+
+import (
+	"github.com/formancehq/go-libs/v3/aws/iam"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"go.uber.org/fx"
+)
+
+const (
+	S3BucketAWSEnabledFlag       = "s3-bucket-aws-enabled"
+	S3BucketEndpointOverrideFlag = "s3-bucket-endpoint-override"
+)
+
+func AddFlags(flags *pflag.FlagSet) {
+	flags.Bool(S3BucketAWSEnabledFlag, true, "Use AWS S3") // default is true until we add support for more s3 compatible providers
+	flags.String(S3BucketEndpointOverrideFlag, "", "Connect to S3 using a custom endpoint (eg. localstack)")
+}
+
+func FXModuleFromFlags(cmd *cobra.Command, debug bool) fx.Option {
+	options := make([]fx.Option, 0)
+
+	awsEnabled, _ := cmd.Flags().GetBool(S3BucketAWSEnabledFlag)
+	endpointOverride, _ := cmd.Flags().GetString(S3BucketEndpointOverrideFlag)
+	if awsEnabled {
+		options = append(options,
+			fx.Supply(iam.LoadOptionFromCommand(cmd)),
+			awsModule(cmd, endpointOverride),
+		)
+	}
+	return fx.Options(options...)
+}
