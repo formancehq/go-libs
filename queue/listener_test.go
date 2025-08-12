ac954de83ee23b121aec67c60a3d6cdd143212d8
Crimson Thompson <crimson@formance.com>
2025-08-12 18:17:51 +0200

    Use Ginkgo only for testing listener integration with localstack

--- queue/listener_test.go
+++ queue/listener_test.go
@@ -1,201 +1,28 @@
 package queue_test
 
 import (
-	"context"
-	"crypto/md5" //nolint: gosec
-	"encoding/hex"
-	"fmt"
+	"context" //nolint: gosec
+	"os"
 	"testing"
 
-	"github.com/ThreeDotsLabs/watermill-aws/sqs"
-	"github.com/ThreeDotsLabs/watermill/message"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
-	sqsservice "github.com/aws/aws-sdk-go-v2/service/sqs"
-	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
-	"github.com/elgohr/go-localstack"
 	"github.com/formancehq/go-libs/v3/logging"
 	"github.com/formancehq/go-libs/v3/queue"
-	"github.com/google/uuid"
-
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	gomegatypes "github.com/onsi/gomega/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestListener(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Listener Suite")
+func TestNewListenerWorkerCount(t *testing.T) {
+	logger := logging.NewDefaultLogger(os.Stderr, true, true, false)
+	listener, err := queue.NewListener(logger, func(ctx context.Context, meta map[string]string, msg []byte) error { return nil }, 0)
+	require.NotNil(t, err)
+	assert.ErrorContains(t, err, "workerCount")
+	assert.Nil(t, listener)
 }
 
-func initClient() (client *sqsservice.Client, queueURL string, ch <-chan *message.Message, err error) {
-	ctx := logging.TestingContext()
-	l, err := localstack.NewInstance()
-	if err != nil {
-		return nil, "", ch, fmt.Errorf("Could not connect to Docker %w", err)
-	}
-	if err := l.Start(); err != nil {
-		return nil, "", ch, fmt.Errorf("Could not start localstack %w", err)
-	}
-	DeferCleanup(l.Stop)
-
-	region := "us-east-1"
-	endpoint := l.EndpointV2(localstack.SQS)
-
-	//nolint: staticcheck
-	cfg, err := config.LoadDefaultConfig(ctx,
-		config.WithRegion(region),
-		config.WithEndpointResolverWithOptions(aws.EndpointResolverWithOptionsFunc(
-			func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-				return aws.Endpoint{
-					URL:           l.EndpointV2(localstack.SQS),
-					SigningRegion: region,
-				}, nil
-			},
-		)),
-	)
-	if err != nil {
-		return nil, "", ch, fmt.Errorf("Could not load config %w", err)
-	}
-	optFns := []func(o *sqsservice.Options){
-		func(o *sqsservice.Options) {
-			o.BaseEndpoint = &endpoint
-			o.Credentials = credentials.NewStaticCredentialsProvider("dummy", "dummy", "dummy")
-		},
-	}
-
-	subscriber, err := sqs.NewSubscriber(sqs.SubscriberConfig{
-		AWSConfig: cfg,
-		OptFns:    optFns,
-	}, nil)
-	if err != nil {
-		return nil, "", ch, fmt.Errorf("Failed to create subscriber %w", err)
-	}
-
-	client = sqsservice.NewFromConfig(cfg, optFns...)
-	queueName := uuid.NewString()
-	out, err := client.CreateQueue(ctx, &sqsservice.CreateQueueInput{
-		QueueName: &queueName,
-	})
-	if err != nil {
-		return nil, "", ch, fmt.Errorf("Could not create queue %w", err)
-	}
-	ch, err = subscriber.Subscribe(ctx, queueName)
-	if err != nil {
-		return nil, "", ch, fmt.Errorf("Failed to subscribe %w", err)
-	}
-	return client, *out.QueueUrl, ch, nil
-}
-
-var _ = Describe("SQS Listener", func() {
-	var (
-		client   *sqsservice.Client
-		ch       <-chan *message.Message
-		queueURL string
-		logger   logging.Logger
-		err      error
-	)
-
-	Context("listening", func() {
-		BeforeEach(func() {
-			client, queueURL, ch, err = initClient()
-			Expect(err).To(BeNil())
-
-			logger = logging.NewDefaultLogger(GinkgoWriter, true, true, false)
-		})
-		DescribeTable("can't create listener when config is invalid",
-			func(workerCount int, match string) {
-				listener, err := queue.NewListener(logger, func(ctx context.Context, meta map[string]string, msg []byte) error { return nil }, workerCount)
-				Expect(err).NotTo(BeNil())
-				Expect(err.Error()).To(ContainSubstring(match))
-				Expect(listener).To(BeNil())
-			},
-			Entry("worker count is too small", 0, "workerCount"),
-		)
-		DescribeTable("can't create listener when callback is nil",
-			func(workerCount int, match string) {
-				listener, err := queue.NewListener(logger, nil, workerCount)
-				Expect(err).NotTo(BeNil())
-				Expect(err.Error()).To(ContainSubstring(match))
-				Expect(listener).To(BeNil())
-			},
-			Entry("callback is nil", 1, "callback"),
-		)
-		It("calls the callback function for each message", func(specCtx SpecContext) {
-			ctx, cancel := context.WithCancel(context.Background())
-			defer cancel()
-
-			metadataTagKey := "some-key"
-			expectedValue := "the-value"
-			var callbackCount int
-			var foundKey bool
-			var foundValue string
-			out := make(chan string)
-			fn := func(ctx context.Context, meta map[string]string, msg []byte) error {
-				callbackCount++
-				foundValue, foundKey = meta[metadataTagKey]
-				out <- string(msg)
-				return nil
-			}
-			listener, err := queue.NewListener(logger, fn, 2)
-			Expect(err).To(BeNil())
-			listener.Listen(ctx, ch)
-
-			body := "hi"
-			res, err := client.SendMessage(specCtx, &sqsservice.SendMessageInput{
-				QueueUrl:    &queueURL,
-				MessageBody: &body,
-				MessageAttributes: map[string]types.MessageAttributeValue{
-					metadataTagKey: {
-						DataType:    aws.String("String"),
-						StringValue: aws.String(expectedValue),
-					},
-				},
-			})
-			Expect(err).To(BeNil())
-			Eventually(out).Should(Receive(Message(*res.MD5OfMessageBody)))
-			Expect(callbackCount).To(Equal(1))
-			Expect(foundKey).To(BeTrue())
-			Expect(foundValue).To(Equal(expectedValue))
-		})
-	})
-})
-
-type MessageMatcher struct {
-	md5OfBody string
-	err       error
-}
-
-func (m *MessageMatcher) Match(actual any) (success bool, err error) {
-	msg, ok := actual.(string)
-	if !ok {
-		m.err = fmt.Errorf("expected type string but got %T", actual)
-		return false, nil
-	}
-
-	//nolint: gosec
-	hash := md5.Sum([]byte(msg))
-	sum := hex.EncodeToString(hash[:])
-	if sum != m.md5OfBody {
-		m.err = fmt.Errorf("expected: %q, got: %q", m.md5OfBody, sum)
-		return false, nil
-	}
-	return true, nil
-}
-
-func (m *MessageMatcher) FailureMessage(_ any) (message string) {
-	return fmt.Sprintf("message does not match expectations: %s", m.err)
-}
-
-func (m *MessageMatcher) NegatedFailureMessage(_ any) (message string) {
-	return "message should not match"
-}
-
-var _ gomegatypes.GomegaMatcher = (*MessageMatcher)(nil)
-
-func Message(md5OfBody string) gomegatypes.GomegaMatcher {
-	return &MessageMatcher{
-		md5OfBody: md5OfBody,
-	}
+func TestNewListenerInvalidCallback(t *testing.T) {
+	logger := logging.NewDefaultLogger(os.Stderr, true, true, false)
+	listener, err := queue.NewListener(logger, nil, 1)
+	require.NotNil(t, err)
+	assert.ErrorContains(t, err, "callback")
+	assert.Nil(t, listener)
 }
