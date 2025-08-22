811e3ccb36a934757c31a1fd97b055109094f58c
Crimson Thompson <crimson@formance.com>
2025-08-22 11:40:26 +0200

    Add localstack tooling for use in integration tests

---
+++ testing/platform/localstacktesting/localstack_test.go
@@ -0,0 +1,53 @@
+package localstacktesting
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/formancehq/go-libs/v3/logging"
+	"github.com/formancehq/go-libs/v3/testing/docker"
+	"github.com/formancehq/go-libs/v3/testing/utils"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var srv *LocalstackServer
+
+func TestMain(m *testing.M) {
+	utils.WithTestMain(func(t *utils.TestingTForMain) int {
+		pool := docker.NewPool(t, logging.Testing())
+		srv = CreateLocalstackServer(t, pool)
+		return m.Run()
+	})
+}
+
+func TestLocalstack(t *testing.T) {
+	u, err := url.Parse(srv.Endpoint())
+	require.NoError(t, err)
+	assert.Equal(t, u.Port(), srv.GetPort())
+
+	for i := 0; i < 10; i++ {
+		t.Run(fmt.Sprintf("test%d", i), func(t *testing.T) {
+			t.Parallel()
+
+			ctx := context.Background()
+			cfg, err := srv.GetAWSConfig(ctx)
+			require.NoError(t, err)
+
+			s3Opts := append([]func(o *s3.Options){}, func(o *s3.Options) {
+				o.UsePathStyle = true
+				o.BaseEndpoint = aws.String(srv.Endpoint())
+			})
+			s3Client := s3.NewFromConfig(cfg, s3Opts...)
+			result, err := s3Client.ListBuckets(ctx, &s3.ListBucketsInput{})
+			require.NoError(t, err)
+			require.NotNil(t, result)
+			assert.Len(t, result.Buckets, 0)
+		})
+	}
+}
