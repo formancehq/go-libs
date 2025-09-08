d4faefe07c4071f68b12dc9e05794bfaad4ad736
Alix Bott <bott.alix@gmail.com>
2025-09-08 13:29:24 +0000

    fix: make localstack respect DOCKER_HOST for rootless setups (#513)
    * make localstack respect DOCKER_HOST for rootless setups
    
    * fix case

--- queue/localstack_integration_test.go
+++ queue/localstack_integration_test.go
@@ -31,7 +31,11 @@ func TestListener(t *testing.T) {
 
 func initClient() (client *sqsservice.Client, queueURL string, ch <-chan *message.Message, err error) {
 	ctx := logging.TestingContext()
-	l, err := localstack.NewInstance()
+	fromEnvOpt, err := localstack.WithClientFromEnv()
+	if err != nil {
+		return nil, "", ch, fmt.Errorf("Could not connect to Docker %w", err)
+	}
+	l, err := localstack.NewInstance(fromEnvOpt)
 	if err != nil {
 		return nil, "", ch, fmt.Errorf("Could not connect to Docker %w", err)
 	}
