a39adb7105d8cb2a4af43da9a5469370a8474814
Sylvain Rabot <sylvain@formance.com>
2025-11-11 21:30:38 +0100

    chore: update golangci-lint config
    Signed-off-by: Sylvain Rabot <sylvain@formance.com>

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2505";
+    nixpkgs-unstable.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
 
     nur = {
       url = "github:nix-community/NUR";
@@ -10,7 +11,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, nur }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, nur }:
     let
       goVersion = 25;
 
@@ -20,7 +21,6 @@
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-
       forEachSupportedSystem = f:
         nixpkgs.lib.genAttrs supportedSystems (system:
           let
@@ -29,8 +29,13 @@
               overlays = [ self.overlays.default nur.overlays.default ];
               config.allowUnfree = true;
             };
+            pkgs-unstable = import nixpkgs-unstable {
+              inherit system;
+              overlays = [ self.overlays.default nur.overlays.default ];
+              config.allowUnfree = true;
+            };
           in
-          f { pkgs = pkgs; system = system; }
+          f { pkgs = pkgs; pkgs-unstable = pkgs-unstable; system = system; }
         );
     in
     {
@@ -38,18 +43,18 @@
         go = final."go_1_${toString goVersion}";
       };
 
-      devShells = forEachSupportedSystem ({ pkgs, system }:
+      devShells = forEachSupportedSystem ({ pkgs, pkgs-unstable, system }:
         {
           default = pkgs.mkShell {
-            packages = with pkgs; [
-              go
-              gotools
-              golangci-lint
-              ginkgo
-              mockgen
-              gomarkdoc
-              just
-              goperf
+            packages = with pkgs; with pkgs-unstable; [
+              pkgs.go
+              pkgs.gotools
+              pkgs.ginkgo
+              pkgs.mockgen
+              pkgs.gomarkdoc
+              pkgs.just
+              pkgs.goperf
+              pkgs-unstable.golangci-lint
             ];
           };
         }
